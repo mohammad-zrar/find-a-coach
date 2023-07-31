585f7de4b3f54f3932f0508f3a09477bf5495bfe
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-07-31 22:06:49 +0300

    Caching http response Data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,15 +8,26 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach && !isLoading" to="/register" link>Register as Coach</base-button>
+        <base-button mode="outline" @click="loadCoaches(true)"
+          >Refresh</base-button
+        >
+        <base-button v-if="!isCoach && !isLoading" to="/register" link
+          >Register as Coach</base-button
+        >
       </div>
       <div v-if="isLoading">
         <base-spinner></base-spinner>
       </div>
       <ul v-else-if="hasCoaches">
-        <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-          :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
       </ul>
       <h3 v-else>No coaches found.</h3>
     </base-card>
@@ -74,10 +85,12 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    async loadCoaches() {
+    async loadCoaches(refresh = false) {
       this.isLoading = true;
       try {
-        await this.$store.dispatch('coaches/loadCoaches');
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
       } catch (error) {
         this.error = error.message || 'Something went wrong!';
       }
@@ -85,7 +98,7 @@ export default {
     },
     handleError() {
       this.error = null;
-    }
+    },
   },
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,54 +1,60 @@
 export default {
-    async registerCoach(context, data) {
-        const userId = context.rootGetters.userId;
-        const coachData = {
-            firstName: data.first,
-            lastName: data.last,
-            description: data.desc,
-            hourlyRate: data.rate,
-            areas: data.areas,
-        };
-        
-        const response = await fetch(`${process.env.VUE_APP_API_URL}/coaches/${userId}.json`, {
-            method: 'PUT',
-            body: JSON.stringify(coachData)
-        });
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
 
-        // const responseData = await response.json();
+    const response = await fetch(
+      `${process.env.VUE_APP_API_URL}/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      },
+    );
 
-        if (!response.ok) {
-            // error ...
-        }
+    // const responseData = await response.json();
 
-        context.commit('registerCoach', {
-            ...coachData,
-            id: userId,
-        });
-    },
-    async loadCoaches(context) {
-        const response = await fetch(`${process.env.VUE_APP_API_URL}/coaches.json`);
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            const error = new Error(response.message || 'Failed to fetch!');
-            throw error;
-        }
-
-        const coaches = [];
-
-        for (const key in responseData) {
-            const coach = {
-                id: key,
-                firstName: responseData[key].firstName,
-                lastName: responseData[key].lastName,
-                description: responseData[key].description,
-                hourlyRate: responseData[key].hourlyRate,
-                areas: responseData[key].areas,
-            };
-            coaches.push(coach);
-        }
-        
-        context.commit('setCoaches', coaches);
-    
+    if (!response.ok) {
+      // error ...
     }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+    const response = await fetch(`${process.env.VUE_APP_API_URL}/coaches.json`);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(response.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
+  },
 };
