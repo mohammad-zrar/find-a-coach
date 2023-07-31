4aebcdaddefc7a326e309df057f3764246c7fc22
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-07-31 19:44:07 +0300

    Adding http error handling

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -30,7 +30,8 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            // ...
+            const error = new Error(response.message || 'Failed to fetch!');
+            throw error;
         }
 
         const coaches = [];
