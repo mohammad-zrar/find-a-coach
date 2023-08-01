e0eb62aa238b2e7a2e31fef9b18f5b9ecffcdd1e
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-08-01 17:49:52 +0300

    Authentication & routing (incl. Navigation Guards)

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -57,4 +57,11 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
+  logout(context) {
+    context.commit('setUser', {
+      token: null,
+      userId: null,
+      tokenExpiration: null,
+    });
+  },
 };
